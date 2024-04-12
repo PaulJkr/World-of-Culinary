9dbea513fc68474f98095e495e9ca9be2fc00221
PaulJkr <paulomondi027@gmail.com>
2024-04-13 02:42:39 +0300

    back-end config

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 import 'package:recipe_app/configs/constants.dart';
 import 'package:recipe_app/views/customtext.dart';
 import 'package:recipe_app/views/customtextfield.dart';
@@ -40,7 +42,20 @@ class Login extends StatelessWidget {
               backgroundColor: Colors.transparent,
               foregroundColor: appWhiteColor,
               centerTitle: true,
-              actions: const [Icon(Icons.refresh), Icon(Icons.dark_mode)],
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.refresh),
+                  onPressed: () {
+                    // Refresh action
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.dark_mode),
+                  onPressed: () {
+                    // Toggle dark mode action
+                  },
+                ),
+              ],
             ),
           ),
           SingleChildScrollView(
@@ -59,7 +74,10 @@ class Login extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Image.asset(
-                          width: 200, height: 200, 'assets/images/book.jpg'),
+                        'assets/images/book.jpg',
+                        width: 200,
+                        height: 200,
+                      ),
                     ],
                   ),
                   const SizedBox(
@@ -123,7 +141,7 @@ class Login extends StatelessWidget {
                     ],
                   ),
                   MaterialButton(
-                    onPressed: () => Get.toNamed("/registration"),
+                    onPressed: () => Get.toNamed("/dashboard"),
                     color: Colors.transparent,
                     height: 50,
                     minWidth: double.maxFinite,
@@ -135,6 +153,16 @@ class Login extends StatelessWidget {
                       ),
                     ),
                   ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  InkWell(
+                    onTap: () => Get.toNamed("/registration"),
+                    child: Text(
+                      "Go to SignUp",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -142,5 +170,23 @@ class Login extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<void> remotelogin() async {
+    http.Response response;
+    response = await http.get(Uri.parse(
+        "https://todaysnewspaper.xyz/paul/Recipe/login.php?phonenumber=${phoneNumberController.text.trim()}&password=${passwordController.text.trim()}"));
+    if (response.statusCode == 200) {
+      var serverResponse = json.decode(response.body);
+      var loginStatus = serverResponse['success'];
+      if (loginStatus == 1) {
+        //navigate to dashboard
+        print("Login Success");
+      } else {
+        print("Username/Password is invalid");
+      }
+    } else {
+      print("Server Error ${response.statusCode}");
+    }
   }
 }
