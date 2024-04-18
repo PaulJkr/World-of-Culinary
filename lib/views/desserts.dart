39c5d2fa356bbe1b156c3eabd5db60644154be9f
PaulJkr <paulomondi027@gmail.com>
2024-04-18 19:39:22 +0300

    desserts config

--- lib/views/desserts.dart
+++ lib/views/desserts.dart
@@ -14,20 +14,7 @@ class MyRecipeApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Desserts'),
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/bacc.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: DessertsScreen(),
-        ),
-      ),
+      home: DessertsScreen(),
     );
   }
 }
@@ -42,51 +29,60 @@ class DessertsScreen extends StatelessWidget {
         '2. Cocoa powder',
         '3. Baking soda',
         '4. Salt',
-        '5. Boiling water',
+        '5. Eggs and egg yolks'
       ],
       'instructions': [
         '1. Heat oven and prepare pans: Preheat oven to 350 degrees. Butter two 9-inch round cake pans then line bottom of each with a round of parchment paper. Butter parchment paper. Wrap cake pans with baking strips for the most level cake.',
-        '2. Mix dry ingredients, then mix in boiling water: In a large, heat proof mixing bowl, whisk together granulated sugar brown sugar, cocoa powder, baking soda and salt.',
-        '3. Mix in wet ingredients: Using an electric hand mixer set on low speed blend in vegetable oil and melted butter until combined. Add eggs, egg yolks and vanilla extract and blend just to combined.',
+        '2. Mix dry ingredients, then mix in boiling water: In a large, heat proof mixing bowl, whisk together granulated sugar brown sugar, cocoa powder, baking soda and salt. Allow to cool 5 minutes.',
+        '3. Pour into pans and bake: Divide mixture evenly among 2 prepared pans. Bake in preheated oven until toothpick inserted into center of cake comes out clean, about 29 – 34 minutes.'
       ],
     },
     {
       'title': 'Strawberry Cheesecake',
       'image': 'assets/images/strawberrycheesecake.jpg',
       'ingredients': [
-        '1. Strarwberries',
+        '1. Strawberries',
         '2. Graham crackers',
         '3. Granulated sugar',
         '4. Butter',
-        '5. Cream cheese '
+        '5. Cream cheese'
       ],
       'instructions': [
-        '1. Blend in eggs: Mix in eggs 2 at a time, mixing just until combined after additions. Scrape down bowl.',
-        '2. Add remaining: Blend in sour cream, heavy cream and vanilla just until combined. Tap bowl against countertop about 20 times to release any large air bubbles.',
-        '3. Prepare pan with foil: wrap 2 layers of heavy duty aluminum foil snuggly around outside and up to the top of springform pan',
-        '4. Pour filling over crust: Pour cheesecake filling over graham cracker crust, wiggle pan to level. Place cheesecake in a roasting pan large enough to fit springform pan with extra space around all sides.'
+        '1. Preheat oven to 350 degrees',
+        '2. Make the crust mixture: in a medium mixing bowl stir together graham crackers with granulated sugar. Pour in butter and stir until evenly moistened.',
+        '3. Press into pan: Press graham crackers into an even layer along the bottom and about halfway up the sides of a 9-inch springform pan.',
+        '4. Bake to set: Bake in preheated oven 9-minutes, remove and let cool on a wire rack.'
       ],
     },
   ];
 
-  DessertsScreen({Key? key});
+  DessertsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: desserts.length,
-      itemBuilder: (context, index) {
-        return DessertsCard(
-          title: desserts[index]['title']!,
-          image: desserts[index]['image']!,
-          ingredients: List<String>.from(
-            desserts[index]['ingredients'] as List<dynamic>,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Desserts'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bacc.jpg'),
+            fit: BoxFit.cover,
           ),
-          instructions: List<String>.from(
-            desserts[index]['instructions'] as List<dynamic>,
-          ),
-        );
-      },
+        ),
+        child: ListView.builder(
+          itemCount: desserts.length,
+          itemBuilder: (context, index) {
+            return DessertsCard(
+              title: desserts[index]['title']!,
+              image: desserts[index]['image']!,
+              ingredients: List<String>.from(desserts[index]['ingredients']),
+              instructions: List<String>.from(desserts[index]['instructions']),
+            );
+          },
+        ),
+      ),
     );
   }
 }
@@ -168,23 +164,31 @@ class RecipePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset(
-            image,
-            width: double.infinity,
-            height: 200.0,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bacc.jpg'),
             fit: BoxFit.cover,
           ),
-          _buildSection('Ingredients:', _buildList(ingredients)),
-          _buildSection('Instructions:', _buildList(instructions)),
-        ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              image,
+              width: double.infinity,
+              height: 200.0,
+              fit: BoxFit.cover,
+            ),
+            _buildSection('Ingredients:', ingredients),
+            _buildSection('Instructions:', instructions),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _buildSection(String title, Widget content) {
+  Widget _buildSection(String title, List<String> content) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
@@ -197,24 +201,18 @@ class RecipePage extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-          SizedBox(height: 8.0),
-          content,
+          const SizedBox(height: 8.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: content
+                .map((item) => Text(
+                      '- $item',
+                      style: const TextStyle(fontSize: 16.0),
+                    ))
+                .toList(),
+          ),
         ],
       ),
-    );
-  }
-
-  Widget _buildList(List<String> items) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return Text(
-          items[index],
-          style: TextStyle(
-              fontSize: 16.0, fontStyle: FontStyle.italic), // Change here
-        );
-      },
     );
   }
 }
